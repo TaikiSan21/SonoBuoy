a2e21508ab2aa9dd924e87392ba17af2e3fb6426
TaikiSan21 <taiki.sakai@noaa.gov>
2017-11-02 08:37:30 -0700

    mini

--- setteCal.R
+++ setteCal.R
@@ -2,27 +2,32 @@ library(PAMsbuoy)
 library(dplyr)
 library(ggplot2)
 library(geosphere)
+library(viridisLite)
 source('loadGpsDifar.R')
 source('SonoBuoyFunctions.R')
 
 dbFile <- './Data/HICEAS_2017/Sette/Database/1706_pg11511_sb_35_20170914.sqlite3'
+dbFile <- './Data/HICEAS_2017/Sette/Database/1706_pg11511_sb_45_20171008.sqlite3'
+
 pamDb <- loadDB(dbFile)
 pamData <- formatStation(pamDb)
 myData <- loadGpsDifar(dbFile) %>% 
       mutate(RealBearing = bearing(cbind(BuoyLongitude, BuoyLatitude), cbind(BoatLongitude, BoatLatitude)) %% 360,
-      AngleError = (RealBearing - DIFARBearing) %% 360,
-      AngleError = sapply(AngleError, function(x) {
-            if(is.na(x)) {x}
-            else if(abs(x) <= abs(x-360)) {x}
-            else {x-360}
-      })) %>% 
+      AngleError = (RealBearing - DIFARBearing)) %>% #,
+      # AngleError = sapply(AngleError, function(x) {
+      #       if(is.na(x)) {x}
+      #       else if(abs(x) <= abs(x-360)) {x}
+      #       else {x-360}
+      # })) %>% 
       select(-Latitude, -Longitude, -snr, -RMS, -ZeroPeak, -PeakPeak, -SEL, -TrueBearing, -TriggerName,
              -PCLocalTime, -PCTime, -TrackedGroup, -MatchedAngles) %>% 
       filter(DifarFrequency < 900)
 
-myData %>% filter(Channel==1) %>% 
-      mutate(AngleError = mapply(errorTransform, DIFARBearing + 180, RealBearing)) %>%
-      ggplot(aes(x=RealBearing, y=AngleError)) + geom_point(aes(color=as.factor(Channel))) +
-      xlim(0,360)
+myData %>% 
+      ggplot(aes(x=RealBearing, y=AngleError %% 360)) + geom_point(aes(color=DifarFrequency < 500)) +
+      facet_wrap(~Channel) + scale_color_gradientn(colors=viridis(256), limits=c(50, 62))
+
+# moving average of calibration
+
 
 
